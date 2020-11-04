c1877697fc89050d9790c1048fc46d07ecae24b7
火柴 <crazywork@foxmail.com>
2020-11-04 18:25:54 +0800

    feat:debugging amap

--- app/src/main/java/com/zzmzrj/water/DeviceFragment.kt
+++ app/src/main/java/com/zzmzrj/water/DeviceFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import com.amap.api.maps2d.AMap
+import com.amap.api.maps2d.AMapOptions
+import com.amap.api.maps2d.CameraUpdateFactory
+import com.amap.api.maps2d.UiSettings
+import kotlinx.android.synthetic.main.map_view.*
 
 
 class DeviceFragment : Fragment() {
 
-
+    private lateinit var aMap: AMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,61 @@ class DeviceFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        initView(savedInstanceState)
+    }
 
+
+
+
+    private fun initView(savedInstanceState: Bundle?){
+        mapView.onCreate(savedInstanceState)
+        // aMap =
+
+        // myLocationStyle = MyLocationStyle()
+        // myLocationStyle.strokeColor(Color.TRANSPARENT) // 设置圆形的边框颜色
+        //
+        // myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0)) // 设置圆形的填充颜色
+        //
+        // myLocationStyle.strokeWidth(1.0f) // 设置圆形的边框粗细
+        //
+        // aMap.setMyLocationStyle(myLocationStyle)
+        // aMap.setMyLocationStyle(myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE))
+        mapView.map.uiSettings.isMyLocationButtonEnabled = false // 设置默认定位按钮是否显示
+
+        mapView.map.isMyLocationEnabled = true // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+
+        // val uiSettings = aMap.uiSettings
+
+        mapView.map.uiSettings.isZoomControlsEnabled = true
+        mapView.map.uiSettings.zoomPosition = AMapOptions.ZOOM_POSITION_RIGHT_BUTTOM;
+
+        // aMap.mapType = AMap.MAP_TYPE_NORMAL
+        // aMap.moveCamera(CameraUpdateFactory.zoomTo(10f)) //地图默认缩放比例
+
+
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        // CameraPosition = mMapView
+        super.onDestroy()
+        mapView.onDestroy()
     }
 
 
--- app/src/main/java/com/zzmzrj/water/InspectionFragment.kt
+++ app/src/main/java/com/zzmzrj/water/InspectionFragment.kt
@@ -1,17 +1,20 @@
 package com.zzmzrj.water
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.amap.api.maps2d.AMap
+import com.amap.api.maps2d.AMap.MAP_TYPE_NORMAL
+import com.amap.api.maps2d.CameraUpdateFactory
+import com.amap.api.maps2d.model.MyLocationStyle
 import kotlinx.android.synthetic.main.map_view.*
+
 
 class InspectionFragment:Fragment() {
 
-    private lateinit var aMap: AMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,18 +30,8 @@ class InspectionFragment:Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         // inspection_text.text = "这是我的内容"
-        initView(savedInstanceState)
     }
 
 
-    private fun initView(savedInstanceState: Bundle?){
-        if (savedInstanceState != null) {
-            mMapView.onCreate(savedInstanceState)
-            aMap = mMapView.map
-        }else{
-            Log.d("savedInstanceState", savedInstanceState.toString())
-        }
-
-    }
 
 }
